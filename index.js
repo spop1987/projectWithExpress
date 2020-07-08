544c3ed162401ff565a0a66cf0bd4a1340d8fd86
Sergio Pastor Ontiveros Pérez <sergio.ontiveros.perez@outlook.com>
2020-07-08 12:00:23 -0300

    Chore: the return message was altered

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/test', (req, res) => {
-    return res.json({hello: "World"})
+    return res.json({hello: "World, How you doing?"})
 })
 
 app.listen(3333)
